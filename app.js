f1f394988dc1d25471c4ace5d8ed3006489b61e2
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-03-29 22:24:36 +0300

    small update from strycture

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const hbr = require('express-handlebars');
-
-const homeController = require('./controllers/homeController');
-
-const handlebars = hbr.create({
-    extname: '.hbs'
-});
 
 const app = express();
-
-app.engine('.hbs', handlebars.engine);
-app.set('view engine', '.hbs');
 
 app.use(express.urlencoded({extended: false}));
 app.use('/static', express.static('static'));
 
-app.get('/', homeController)
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/static/index.html');
+})
 
 app.listen(3005);
