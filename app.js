59000b1efb3de53820884cd350fc8fcd1fa183ec
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-04-16 19:11:55 +0300

    testing adding https for server

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const https = require('https');
 const express = require('express');
+const fs = require('fs');
+
+const options = {
+    key: fs.readFileSync('../key.pem'),
+    cert: fs.readFileSync('../cert.pem')
+}
 
 const app = express();
+
+https.createServer(options, app).listen(80, () => console.log('Server listen'));
 
 app.use(express.urlencoded({extended: false}));
 app.use('/static', express.static('static'));
@@ -13,4 +22,4 @@ app.get('/.well-known/pki-validation/fileauth.txt', (req, res) => {
     res.sendFile(__dirname + '/static/fileauth.txt');
 })
 
-app.listen(3005);
+// app.listen(3000);
